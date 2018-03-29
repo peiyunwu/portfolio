8e2d98bed1fc24b0ae71dfa7d137f19f580d267c
peiyunwu <zoeyunwu@gmail.com>
2018-03-29 15:50:51 +0800

    20180329

--- js/base.js
+++ js/base.js
@@ -50,3 +50,11 @@ function toggleFunction() {
 //   $(function() {
 //       $('body').removeClass('fade-out');
 // });
+
+$(document).ready(function(){
+
+/*! Fades in page on load */
+$('body').css('display', 'none');
+$('body').fadeIn(100);
+
+});
